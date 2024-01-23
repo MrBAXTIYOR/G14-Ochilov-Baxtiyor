c26f2577578f2754e02ff479c8748f078baccc3a
MrBAXTIYOR <githup79@gmail.com>
2024-01-23 14:41:05 +0500

    dvdfvf

--- main/js/main.js
+++ main/js/main.js
@@ -17,47 +17,57 @@ addTodoForm.addEventListener("submit", function (e) {
 
     .then((data) => {
       alert(data.message);
-      console.log(data.todo.userId);
-      info.innerHTML = `
-      <li>Todo: ${data.todo.task} <i class="fa-solid fa-user-pen"  onclick="editUser('${data.todo.userId}')"> </i> <i class="fa-solid fa-trash ii" onclick="deliteUser('${data.todo.userId}')"></i></li>`;
+      rederData();
     })
     .catch((error) => console.error("Add Todo Error:", error));
 });
-function delitetodo(id) {
-  fetch(` https://todo-for-n92.cyclic.app/todos/:${id}`, {
-    method: "DELETE",
+
+async function rederData() {
+  let data = await fetch("https://todo-for-n92.cyclic.app/todos/all", {
+    method: "GET",
     headers: {
       "Content-Type": "application/json",
       "x-access-token": localStorage.getItem("token"),
     },
-  })
-    .then((response) => console.log(response))
-
-    .catch((error) => console.error("Add Todo Error:", error));
-}
-
-async function deliteUser(id) {
-  let res = await fetch(Main__IP + "/todo/" + id, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-    },
   });
-
-  renderData();
+  let res = await data.json();
+  console.log(res);
+  info.innerHTML = "";
+  res.allTodos.forEach((element) => {
+    info.innerHTML += `
+  <li>Todo: ${element.task} <i class="fa-solid fa-user-pen"  onclick="editUser('${element._id}')"> </i> <i class="fa-solid fa-trash ii" onclick="deliteUser('${element._id}')"></i></li>`;
+  });
 }
 
 async function editUser(id) {
-  console.log(id);
-  const promptp = prompt("Are you sure you want to edit this user");
-  let res = await fetch(Main__IP + "/todo/" + id, {
+  let prom = prompt("Please enter your todo !!!");
+  fetch(`https://todo-for-n92.cyclic.app/todos/${id}`, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
+      "x-access-token": localStorage.getItem("token"),
     },
-    body: JSON.stringify({
-      todo: promptp,
-    }),
-  });
-  renderData();
+    body: JSON.stringify({ task: prom }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      alert(data.message);
+      rederData();
+    })
+    .catch((error) => console.error("Update Todo Error:", error));
+}
+function deliteUser(todoId) {
+ 
+  fetch(`https://todo-for-n92.cyclic.app/todos/${todoId}`, {
+    method: "DELETE",
+    headers: {
+      "x-access-token": localStorage.getItem("token"),
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      alert(data.message);
+      rederData();
+    })
+    .catch((error) => console.error("Delete Todo Error:", error));
 }
